84ace06427603aa24c17223ead4ad2cd0f1ebc37
lucastgl <lucastgarcialauman@gmail.com>
2023-11-20 19:03:35 -0300

    primeras implementaciones de la carpeta redux y Provider en App.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import { Provider } from 'react-redux';
 import './App.css';
 import PaginaInicio from "./paginas/Inicio.pagina";
 import PaginaFavoritos from "./paginas/Favoritos.pagina";
 import PaginaDetalle from "./paginas/Detalle.pagina";
 import Encabezado from "./componentes/layout/encabezado.componente";
+import store from './redux/store';
 
 function App() {
   return (
     <div className="App">
-      <Encabezado />
-      <Routes>
-        <Route path="/" element={<PaginaInicio />} />
-        <Route path="favoritos" element={<PaginaFavoritos />} />
-        <Route path="detalle" element={<PaginaDetalle />} />
-      </Routes>
+      <Provider store={store}>
+        <Encabezado />
+        <Routes>
+          <Route path="/" element={<PaginaInicio />} />
+          <Route path="favoritos" element={<PaginaFavoritos />} />
+          <Route path="detalle/:characterId" element={<PaginaDetalle />} />
+        </Routes>
+      </Provider>
     </div>
   );
 }
